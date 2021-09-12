c1ecf2b8087813efa93bd66af3c1381d5d65786a
Jonagarme <garciajonathan268@gmail.com>
2021-09-12 11:57:47 -0500

    useEffect unmount

--- src/components/02-useEffect/Message.jsx
+++ src/components/02-useEffect/Message.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export const Message = () => {
+  useEffect(() => {
+    console.log("componente montado");
+    return () => {
+      console.log("componente desmontado");
+    };
+  }, []);
+
   return (
     <div>
       <h3>Eres genial</h3>
